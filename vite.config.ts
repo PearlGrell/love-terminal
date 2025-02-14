49307f8c93009d31da4d1c81e5f4361ec9704c1e
PearlGrell <aryantrivedi.lko@outlook.com>
2025-02-14 14:49:23 +0530

    final

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/valen/'
-  }
-
-  return config
-})
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+  },
+});
